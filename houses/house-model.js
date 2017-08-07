ae5ede0c7f97883c83bef7289afb80ac9797fc21
Diego Mendoza <dmendoza@inflexionsoftware.com>
2017-08-06 22:48:02 -0500

    Ajuste en los campos obligatorios

--- houses/house-model.js
+++ houses/house-model.js
@@ -7,7 +7,7 @@ var houseSchema = new Schema({
   userId:    { type: mongoose.Schema.Types.ObjectId, required: true },
   title:    { type: String, required: true  },
   price:    { type: Number },
-  priceType:    { type: String, required: true, enum:
+  priceType:    { type: String, enum:
     ['Mensual', 'Semanal', 'Dia', 'Noche', 'Compra', 'Otro']},
   propertyType:    { type: String, required: true, enum:
     ['Casa', 'Departamento', 'Oficina', 'Terreno', 'Salon', 'Otro']},
