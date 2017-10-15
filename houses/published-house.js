4265a257bd5ce08c94d80920b456c1a0cf818a5d
Diego Mendoza <dmendoza@inflexionsoftware.com>
2017-10-14 22:25:31 -0500

    Diseñe la consulta para el servicio que va a obtener el listado de casas que se mostraba en el Home, incluyendo todas las opciones de filtros.

--- houses/published-house.js
+++ houses/published-house.js
@@ -42,3 +42,40 @@ exports.getPublishedHouses = function(req, res) {
     );
     
 };
+
+/*
+db.getCollection('houses').aggregate(
+        [
+            {
+                //Incluye la información de los archivos
+                $lookup:
+                {
+                    from: 'files',
+                    localField: 'files',
+                    foreignField: '_id',
+                    as: 'filesData'
+                }
+            },
+            {
+                $match: 
+                {
+                    //Solo las casas publicadas
+                    status: 'Publicado',
+                    propertyType: 'Casa',
+                    operationType: 'Venta',
+                    price: { $gt: 1000, $lt: 5000 },
+                    $or: [ { title: /prueba/i } , { summary: /prueba/i }, 
+                        { 'address.address': /prueba/i }, { 'contact.name': /prueba/i } 
+                    ]
+                }
+            },
+            { 
+                $sort : 
+                { 
+                    //Ordenadas por la última fecha de modificación  
+                    lastModification : -1  
+                } 
+            }
+        ])
+
+*/
